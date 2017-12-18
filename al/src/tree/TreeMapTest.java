e536da67d27b0121924ba0adae83875ca46a4264
jiangwang <577057928@qq.com>
2017-12-18 21:33:30 +0800

    【无】

--- al/src/tree/TreeMapTest.java
+++ al/src/tree/TreeMapTest.java
@@ -8,7 +8,22 @@ import java.util.TreeMap;
  */
 public class TreeMapTest {
     public static void main(String[] args) {
-        TreeMap<String,String> treeMap = new TreeMap<String, String>();
-        treeMap.put("","");
+        TreeMap<Integer,String> treeMap = new TreeMap<Integer, String>();
+        treeMap.put(20,"twenty");
+        treeMap.put(5,"five");
+        treeMap.put(40,"forty");
+        treeMap.put(1,"one");
+        treeMap.put(10,"ten");
+        treeMap.put(30,"thirty");
+        treeMap.put(60,"sixty");
+        treeMap.put(8,"eight");
+        treeMap.put(15,"fif");
+        treeMap.put(7,"seven");
+        treeMap.put(6,"six");
+        treeMap.put(9,"nine");
+
+        System.out.println(treeMap.toString());
+
+        treeMap.remove(5);
     }
 }
