d73889db2f855272c48ef58399db1eea5a7ad383
jiangwang3 <jiangwang3@jd.com>
2017-12-28 17:26:49 +0800

    【快速排序】代码初步完成

--- leetcode/src/easy/number/PascalTriangle.java
+++ leetcode/src/easy/number/PascalTriangle.java
@@ -12,7 +12,11 @@ public class PascalTriangle {
         List<List<Integer>> lists = generate(3);
         System.out.println(lists.toString());
     }
-
+    /**
+     *@Author: jiangwang
+     *@Description: O(N方）
+     *@Date: 17:05 2017/12/28
+     */
     public static List<List<Integer>> generate(int numRows) {
         List<List<Integer>> listList = new ArrayList<List<Integer>>();
 
@@ -26,8 +30,7 @@ public class PascalTriangle {
             List<Integer> lastList = listList.get(i-2);
             list.add(1);
             for(int j=1;j<lastList.size();j++){
-                int num = lastList.get(j-1)+lastList.get(j);
-                list.add(num);
+                list.add(lastList.get(j-1)+lastList.get(j));
             }
             list.add(1);
             listList.add(list);
