4e5ba2b62af35bb2aa0e19a284c5afe6b40ada5d
jiangwang <577057928@qq.com>
2018-05-06 09:45:06 +0800

    【rmi】demo

---
+++ gof/src/singleton/LazySingleton3.java
@@ -0,0 +1,24 @@
+package singleton;
+
+public class LazySingleton3 {
+
+    private static boolean initialized = false;
+
+    private LazySingleton3() {
+        synchronized (LazySingleton3.class) {
+            if (!initialized) {
+                initialized = !initialized;
+            } else {
+                throw new RuntimeException("单例已被破坏");
+            }
+        }
+    }
+
+    static class SingletonHolder {
+        private static final LazySingleton3 instance = new LazySingleton3();
+    }
+
+    public static LazySingleton3 getInstance() {
+        return SingletonHolder.instance;
+    }
+}
