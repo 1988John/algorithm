341f1f4b4d4f55f0c2ff43fcf499769563f70539
jiangwang3 <jiangwang3@jd.com>
2018-10-30 16:56:48 +0800

    【lru】简单实现

---
+++ leetcode/src/lru/LRUCache.java
@@ -0,0 +1,20 @@
+package lru;
+
+import java.util.LinkedList;
+
+class LRUCache {
+
+    private LinkedList linkedList;
+
+    public LRUCache(int capacity) {
+        
+    }
+    
+    public int get(int key) {
+        return key;
+    }
+    
+    public void put(int key, int value) {
+        
+    }
+}
